4c9e978e67b9fdaf16e2ef4669f106d7f00a2627
thehenrylam <the.henry.lam1@gmail.com>
2023-10-31 21:29:19 -0400

    Removed the app_config.sh and replaced it with a TEMPLATE file for users to template on their own

---
+++ app_config.TEMPLATE.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+KEYWORD="Nexus"
+IMAGE_NAME="nexus_image"
+CONTAINER_NAME="nexus"
+VOLUME_NAME="${CONTAINER_NAME}_data"
--- app_config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-KEYWORD="Nexus"
-IMAGE_NAME="nexus_image"
-CONTAINER_NAME="nexus_McAws"
-VOLUME_NAME="nexus_McAws_data"
